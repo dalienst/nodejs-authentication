65cadc925d6ee417b2ae999325f3152595da74d5
dalienst <oduordalienst@gmail.com>
2024-03-26 07:41:18 +0300

    add cors

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.listen(PORT, () => {
 })
 
 app.use(cors({
-    origin: ["http://localhost:4000"],
+    origin: ["http://localhost:4000", "http://localhost:3000",],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
 }))
